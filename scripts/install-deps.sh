b3af8a854e2ac4a16ca9a5a88a196f1244eefb27
Jianshen Liu <468515+ljishen@users.noreply.github.com>
2022-07-23 03:37:34 +0000

    Update copyright holder

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # MIT License
 #
-# Copyright (c) 2022 Jianshen Liu
+# Copyright (c) 2022 University of California
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
